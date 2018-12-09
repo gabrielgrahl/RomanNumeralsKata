607fd539e7e3bb674e879ac67a61c490f054edc6
Gabriel Grahl <gabriel.oechsler@hbsis.com.br>
2018-12-09 13:22:04 -0200

    Initial commit

---
+++ DojoTDD/RomanNumeralsConverter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DojoTDD.Tests
+{
+    public class RomanNumeralsConverter
+    {
+       public int ConverterNumerosRomanos(string numeros)
+        {
+            var retorno = 0;
+
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] == 'I')
+                {
+                    retorno += 1;
+                }
+                if (numeros[i] == 'V')
+                {
+                    if (i > 0 && numeros[i - 1] == 'I')
+                    {
+                        retorno += 3;
+                    }
+                    else
+                    {
+                        retorno += 5;
+                    }
+                }
+                if (numeros[i] == 'X')
+                {
+                    if (i > 0 && numeros[i - 1] == 'I')
+                    {
+                        retorno += 8;
+                    }
+                    else
+                    {
+                        retorno += 10;
+                    }
+                }
+                if (numeros[i] == 'C')
+                {
+                    if (i > 0 && numeros[i - 1] == 'X')
+                    {
+                        retorno += 80;
+                    }
+                    else
+                    {
+                        retorno += 100;
+                    }
+                }
+                if (numeros[i] == 'L')
+                {
+                    if (i > 0 && numeros[i - 1] == 'X')
+                    {
+                        retorno += 30;
+                    }
+                    else
+                    {
+                        retorno += 50;
+                    }                   
+                }
+                if (numeros[i] == 'D')
+                {
+                    if (i > 0 && numeros[i - 1] == 'C')
+                    {
+                        retorno += 300;
+                    }
+                    else
+                    {
+                        retorno += 500;
+                    }                 
+                }
+                if (numeros[i] == 'M')
+                {
+                    if (i > 0 && numeros[i - 1] == 'C')
+                    {
+                        retorno += 800;
+                    }
+                    else
+                    {
+                        retorno += 1000;
+                    }                   
+                }
+            }
+            
+            return retorno;
+        }     
+    }
+}
---
+++ DojoTDD/Tests/RomanNumeralsConverterTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace DojoTDD.Tests
+{
+    public class RomanNumeralsConverterTest
+    {
+        private RomanNumeralsConverter _romanNumeralsConverter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _romanNumeralsConverter = new RomanNumeralsConverter();
+        }
+
+        [Test]
+        public void deve_retornar_1()
+        {
+            _romanNumeralsConverter.ConverterNumerosRomanos("I").Should().Be(1);
+        }
+
+        [Test]
+        public void deve_retornar_5()
+        {
+            _romanNumeralsConverter.ConverterNumerosRomanos("V").Should().Be(5);
+        }
+
+        [Test]
+        public void deve_retornar_10()
+        {
+            _romanNumeralsConverter.ConverterNumerosRomanos("X").Should().Be(10);
+        }
+
+        [Test]
+        public void deve_retornar_2()
+        {
+            _romanNumeralsConverter.ConverterNumerosRomanos("II").Should().Be(2);
+        }
+
+        [Test]
+        public void deve_retornar_20()
+        {
+            _romanNumeralsConverter.ConverterNumerosRomanos("XX").Should().Be(20);
+        }
+
+        [TestCase("XXII", 22)]
+        [TestCase("XI", 11)]
+        [TestCase("XV", 15)]
+        [TestCase("IV", 4)]
+        [TestCase("CCCXCIX", 399)]
+        [TestCase("MCMLXXXIV", 1984)]
+        [TestCase("DIX", 509)]
+        public void deve_somar_dois_numeros(string str, int ret)
+        {
+            _romanNumeralsConverter.ConverterNumerosRomanos(str).Should().Be(ret);
+        }
+
+
+    }
+}
